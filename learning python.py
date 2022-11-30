3ed7c8846d0500a023e215c677912ffb416eb028
Ac0st4 <117885632+Ac0st4@users.noreply.github.com>
2022-11-30 13:46:16 -0500

    Update learning python.py

--- learning python.py
+++ learning python.py
@@ -1,1 +1,41 @@
-print("hello world")
+# For loops
+
+for letter in "Giraffe Academy":
+    print (letter)
+
+
+friends = ["Carlos", "Maria", "Daniela", "Cacaita"]
+
+for friend in friends:
+    print(friend) 
+    
+
+# Exponent function
+print (2**3)
+
+# creating a function()
+
+def raise_to_power(base_num, pow_num):
+    result = 1
+    for index in range (pow_num):
+        result = result * base_num
+    return result
+
+print (raise_to_power(3, 3))
+
+## 2D LISTS &  Nested Loops
+
+number_grid = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [0]
+]
+print(number_grid[0][2])
+
+# Nested for() loops
+
+for row in number_grid:
+    for col in row:
+        print (col)
+
